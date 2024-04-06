a2a5932d9039cc74868fc17c6fdfe9429d459154
pritishdsathe <pritish.sathe01@gmail.com>
2024-04-06 20:05:44 +0530

    Very basics of Python

---
+++ first.py
@@ -0,0 +1,24 @@
+print("First line.")
+print(4+2)
+a= 12 
+b=12
+a1="Pritish"
+print(a+b)
+print(a*b)
+print(a/b)
+print(a-b)
+print(a%b)
+print("Hello", a1)
+if(a>b):
+    print("a is greater than b")
+elif(a<b):
+    print("b is greater than a")
+else:
+    print("a and b are equal")
+
+print("""Good Morning
+India""")
+print("Hi my name is "+a1+" Sathe")
+print(f"Hello, my name is {a1}") #Format String
+print('Hi! Myself "Pritish"')
+print("Hi! Myself 'Pritish Sathe'")
